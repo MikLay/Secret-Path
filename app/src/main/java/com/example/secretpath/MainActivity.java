c225c9c07333ac352e79e899599161a5c551ea8e
Michael Fediuchenko <mixei112299@gmail.com>
2020-06-14 21:58:35 +0300

    [MainActivity] Update MainActivity, Add 3 activities for Levels, Settings and PlayLevel.

--- app/src/main/java/com/example/secretpath/MainActivity.java
+++ app/src/main/java/com/example/secretpath/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.secretpath;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playButtonOnClick(View view) {
+        Log.d(LOG_TAG, "PlayButton clicked");
+        Intent playIntent = new Intent(this, PlayLevelActivity.class);
+        startActivity(playIntent);
+    }
+
+    public void levelsButtonOnClick(View view) {
+        Log.d(LOG_TAG, "LevelsButton clicked");
+        Intent levelsIntent = new Intent(this, LevelsActivity.class);
+        startActivity(levelsIntent);
+    }
+
+    public void settingsButtonOnClick(View view) {
+        Log.d(LOG_TAG, "SettingsButton clicked");
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        startActivity(settingsIntent);
     }
 }
